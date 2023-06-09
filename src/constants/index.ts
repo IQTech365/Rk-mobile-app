252fcb2ad1ab90d64188c048ed91f125c3f527dc
vijay <vijay.kumar_ext@oceantg.com>
2023-06-09 16:20:04 +0530

    installed redux liberaries && added folder structure

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+const CONFIG = {
+    /* Do not add protocol in endpoint for development phase in case dev server is running locally */
+    API_ENDPOINT: 'http://localhost:8000/api/v1',
+  };
+  
+  export default CONFIG;
